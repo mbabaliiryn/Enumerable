f931f92b0ca110b9c714e778ef1d7d2ee1f513d7
mbabali iryn <mbabaliiryn@gmail.com>
2020-03-16 16:01:19 +0300

    implement & test all the required methods in the enumerable module

--- enumerable.rb
+++ enumerable.rb
@@ -1,16 +1,103 @@
-def my_each(num)
-  count = 0
-  while count < num.size
-    yield num[count]
+module Enumerable
+  def my_each
+    return to_enum unless block_given?
 
-    count += 1
+    counter = 0
+    while counter < length
+      yield self[counter]
+      counter += 1
+    end
+    self
   end
-  num
-end
 
-def my_each_with_index
-  letter.my_each_with_index do |item, index|
-    puts "#{item} is a member #{index}"
+  def my_each_with_index
+    return to_enum unless block_given?
+
+    counter = 0
+    while counter < length
+      yield(self[counter], counter)
+      counter += 1
+    end
+    self
   end
-end
 
+  def my_select
+    return to_enum unless block_given?
+
+    counter = 0
+    num = []
+    while counter < length
+      num << self[counter] if yield(self[counter])
+      counter += 1
+    end
+    num
+  end
+
+  def my_all?
+    all = true
+    my_each do |item|
+      all = yield(item)
+      break unless all
+    end
+    all
+  end
+
+  def my_any?
+    any = false
+    my_each do |item|
+      any = yield(item)
+      break if any
+    end
+    any
+  end
+
+  def my_none?
+    any = false
+    my_each do |item|
+      any = yield(item)
+      break if any
+    end
+    !any
+  end
+
+  def my_count
+    counter = 0
+    my_each do |item|
+      counter += 1 if yield(item)
+    end
+    counter
+  end
+
+  def my_map(&proc)
+    counter = 0
+    array2 = []
+    while counter < size
+      array2 << if block_given?
+                  yield(self[counter])
+                else
+                  proc.call(self[counter])
+                end
+      counter += 1
+    end
+    array2
+  end
+
+  def my_inject(*args)
+    init = !args.empty?
+    memo = init ? args[0] : self[0]
+    drop(init ? 0 : 1).my_each do |item|
+      memo = yield(memo, item)
+    end
+    memo
+  end
+
+  # def my_inject(list = nil)
+  #   if list.nil?
+  #     self[0]
+  #     idx = 1
+  #   else
+  #     idx = 0
+  #   end
+  #   self
+  # end
+end
--- test.rb
+++ test.rb
@@ -1,3 +1,13 @@
-# Each method
-num = %w[20 40 30 10 60 70]
-each(num) { |n| puts n }
+# require_relative "enumerable.rb"
+# num = %w[20 40 30 10 61 70 7 9 3 2]
+# p [20, 40, 30, 10, 61, 70, 7, 9, 3, 2].my_each { |item| puts item }
+# num.my_each_with_index { |item, value| puts "#{item} with index #{value}" }
+# p [20, 40, 30, 10, 61, 70, 7, 9, 3, 2].my_select(&:odd?)
+# p num.my_all? { |x| x.length == 3 }
+# p num.my_any?(&:empty?)
+# p num.my_none? { |num| num.length == 10 }
+# p num.my_count { |item| item.length.even? }
+# p num.my_map { |item| item.to_i + 5 }
+# p [20, 40, 30, 10, 61, 70, 7, 9, 3, 2].my_inject(&:+)
+# p [20, 40, 30, 10, 61, 70, 7, 9, 3, 2].my_inject(&:*)
+# p [20, 40, 30, 10, 61, 70, 7, 9, 3, 2].my_map(&:odd?)
