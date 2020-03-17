7e1ac48b71258e7dad015a3344a226dde715bfb3
mbabali iryn <mbabaliiryn@gmail.com>
2020-03-17 16:50:18 +0300

    add more methods to the enumerable module and test it with different test cases

--- enumerable.rb
+++ enumerable.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Metrics/ModuleLength
 module Enumerable
   def my_each
     return to_enum unless block_given?
@@ -33,42 +34,48 @@ module Enumerable
     num
   end
 
-  def my_all?
-    all = true
-    my_each do |item|
-      all = yield(item)
-      break unless all
+  def my_all?(_regx = nil)
+    my_each do |x|
+      return false if block_given? && yield(x)
     end
-    all
+    true
   end
 
-  def my_any?
-    any = false
-    my_each do |item|
-      any = yield(item)
-      break if any
+  def my_any?(_regx = nil)
+    my_each do |x|
+      return true if block_given? && yield(x)
     end
-    any
+    false
   end
 
-  def my_none?
-    any = false
-    my_each do |item|
-      any = yield(item)
-      break if any
+  def my_none?(_regx = nil)
+    my_each do |x|
+      if block_given?
+        return false if yield(x)
+      elsif x
+        return false
+      end
     end
-    !any
+    true
   end
 
-  def my_count
+  def my_count(index = nil)
     counter = 0
-    my_each do |item|
-      counter += 1 if yield(item)
+
+    if index
+      my_each { |x| counter += 1 if x == index }
+      counter
+    elsif block_given?
+      my_each { |x| counter += 1 if yield(x) }
+      counter
+    else
+      length
     end
-    counter
   end
 
   def my_map(&proc)
+    return to_enum(:my_map) unless block_given?
+
     counter = 0
     array2 = []
     while counter < size
@@ -82,22 +89,53 @@ module Enumerable
     array2
   end
 
-  def my_inject(*args)
-    init = !args.empty?
-    memo = init ? args[0] : self[0]
-    drop(init ? 0 : 1).my_each do |item|
-      memo = yield(memo, item)
+  def inject_arg_valid?(*arg)
+    acc = arg[0]&.is_a?(Numeric) ? arg[0] : to_a[0]
+    if arg[0]&.is_a? Symbol
+      operation = arg[0]
+    elsif arg[1]&.is_a? Symbol
+      operation = arg[1]
     end
-    memo
+    [acc, operation]
   end
 
-  # def my_inject(list = nil)
-  #   if list.nil?
-  #     self[0]
-  #     idx = 1
-  #   else
-  #     idx = 0
-  #   end
-  #   self
-  # end
+  def eval_operation?(operation)
+    ':/ :% :-'.include?(operation.to_s)
+  end
+
+  def inject_valid_symbol?
+    lambda do |arg, i|
+      res = (arg.size == 1 && !i.zero?)
+      res ||= arg.size == 2
+      res
+    end
+  end
+
+  def next_index?(arg, index)
+    ret = arg.size == 2 || arg.size.zero?
+    ret &&= index.zero?
+    ret
+  end
+
+  def check_operation(operation, acc, elem)
+    eval_operation?(operation) ? [acc, elem] : [elem, acc]
+  end
+
+  def my_inject(*arg)
+    acc, operation = inject_arg_valid?(*arg)
+    each_with_index do |e, i|
+      if block_given? && arg.size < 2
+        next if next_index?(arg, i)
+
+        acc = yield(acc, e) if acc
+      elsif inject_valid_symbol?.call(arg, i)
+        e, acc = check_operation(operation, acc, e)
+        acc = e.send(operation, acc)
+      end
+      next
+    end
+    acc
+  end
 end
+
+# rubocop:enable Metrics/ModuleLength
--- test.rb
+++ test.rb
@@ -4,10 +4,36 @@
 # num.my_each_with_index { |item, value| puts "#{item} with index #{value}" }
 # p [20, 40, 30, 10, 61, 70, 7, 9, 3, 2].my_select(&:odd?)
 # p num.my_all? { |x| x.length == 3 }
-# p num.my_any?(&:empty?)
-# p num.my_none? { |num| num.length == 10 }
+# [1].all?
+# [1].my_all?
+# %w[a].all?(/\w/)
+# [1].all?(1)
+# [1].any?
+# [1].my_any?
+# [1].any?(Integer)
+# %w[a].any?(/\w/)
+# %w[a].my_any?(/\w/)
+# [1].any?(1)
+# [1].my_any?(1)
+# [1].my_any?(Integer)
+# p "",num.my_none? { |num| num.length == 10 }
+# p [false].my_none?
+# [1].none?(String)
+# [1].my_none?(String)
+# [1].none?(/\w/)
+# [1].my_none?(/\w/)
+# [1].none?(2)
+# [1].my_none?(2)
 # p num.my_count { |item| item.length.even? }
+# p [1, 2, 3].count
+# p [1, 2, 3].count(2)
 # p num.my_map { |item| item.to_i + 5 }
+# p [1].map
+# p [1].my_map
 # p [20, 40, 30, 10, 61, 70, 7, 9, 3, 2].my_inject(&:+)
+# p [1, 2, 3].inject(:+)
+# p [1, 2, 3].my_inject(:+)
 # p [20, 40, 30, 10, 61, 70, 7, 9, 3, 2].my_inject(&:*)
+# p [1, 2, 3].inject(2, :+)
+# p [1, 2, 3].my_inject(2, :+)
 # p [20, 40, 30, 10, 61, 70, 7, 9, 3, 2].my_map(&:odd?)
